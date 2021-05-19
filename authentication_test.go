3effc54cec481ca14ed71740a3425037e70745eb
Guilherme Firmiano <guilherme.firmiano@dafiti.com.br>
2021-05-18 23:49:23 -0300

    print

--- authentication_test.go
+++ authentication_test.go
@@ -1,6 +1,7 @@
 package bankly_test
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -28,6 +29,9 @@ func (s *AuthenticationTestSuite) SetupTest() {
 		ClientID:     bankly.String(os.Getenv("BANKLY_CLIENT_ID")),
 		ClientSecret: bankly.String(os.Getenv("BANKLY_CLIENT_SECRET")),
 	})
+
+	fmt.Println(os.Getenv("BANKLY_CLIENT_ID"))
+	fmt.Println(os.Getenv("BANKLY_CLIENT_SECRET"))
 
 	s.assert.NoError(err)
 
