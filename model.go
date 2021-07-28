31466aa9c63341e82303be11c52b2d3aa6d18ff4
Guilherme Firmiano <guilherme.firmiano@dafiti.com.br>
2021-07-27 22:07:45 -0300

    responde time

--- model.go
+++ model.go
@@ -157,13 +157,13 @@ type CustomersResponse struct {
 	Phone                      Phone          `json:"phone"`
 	Address                    Address        `json:"address"`
 	MotherName                 string         `json:"motherName"`
-	BirthDate                  string         `json:"birthDate"`
+	BirthDate                  time.Time      `json:"birthDate"`
 	IsPoliticallyExposedPerson bool           `json:"isPoliticallyExposedPerson"`
 	Reasons                    []string       `json:"reasons"`
 	Status                     CustomerStatus `json:"status"`
 	Profile                    string         `json:"profile"`
-	CreatedAt                  string         `json:"createdAt"`
-	UpdatedAt                  string         `json:"updatedAt"`
+	CreatedAt                  time.Time      `json:"createdAt"`
+	UpdatedAt                  time.Time      `json:"updatedAt"`
 }
 
 // Phone ...
